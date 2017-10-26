11b76c7cff6c23096f1a6f4d28a11ac48111507e
wang6688 <mcwqg@outlook.com>
2017-10-26 22:11:44 +0800

    第一次提交 测试学习 git

---
+++ SpringBootDemo/src/main/java/com/boot/Starter.java
@@ -0,0 +1,16 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * 主程序开始
+ */
+@SpringBootApplication
+public class Starter {
+	public static void main(String[] args) {
+		SpringApplication.run(Starter.class, args);
+		
+	}
+}
